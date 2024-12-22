9c70d3ca98b38c1868e5e9a09d24984cfa8cada3
Amaan <amaanreigns01@gmail.com>
2024-12-23 02:12:50 +0530

    Major changes

--- services.php
+++ services.php
@@ -12,74 +12,57 @@
 
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
                 <div class="service-item position-relative">
-                    <div class="icon">
-                        <i class="bi bi-credit-card-2-front" style="color: #df1529;"></i>
+                    <div class="service-img">
+                        <img src="assets/img/service/appliance.jpg" alt="appliance">
                     </div>
-                    <a href="service-appliance.php" class="stretched-link">
+                    <a href="service-appliance.php" class="stretched-link text-center">
                         <h3>Appliance Repair</h3>
                     </a>
-                    <p>We offer a wide range of repair solutions.</p>
+                    <div class="text-center mb-4">
+                        <p>We offer a wide range of repair solutions.</p>
+                    </div>
                 </div>
             </div><!-- End Service Item -->
 
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
                 <div class="service-item  position-relative">
-                    <div class="icon">
-                        <i class="bi bi-cash-stack" style="color: #0dcaf0;"></i>
+                    <div class="service-img">
+                        <img src="assets/img/service/electrician.jpg" alt="electrician">
                     </div>
-                    <a href="service-electric.php" class="stretched-link">
-                        <h3>Electrical Repair Service
-                        </h3>
+                    <a href="service-electric.php" class="stretched-link text-center">
+                        <h3>Electrical Repair Service</h3>
                     </a>
-                    <p>We provide top-quality electrical repair services.</p>
+                    <div class="text-center mb-4">
+                        <p>We provide top-quality electrical repair services.</p>
+                    </div>
                 </div>
             </div><!-- End Service Item -->
 
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
                 <div class="service-item position-relative">
-                    <div class="icon">
-                        <i class="bi bi-calendar4-week" style="color: #fd7e14;"></i>
+                    <div class="service-img">
+                        <img src="assets/img/service/plumber.jpg" alt="plumber">
                     </div>
-                    <a href="service-plumber.php" class="stretched-link">
+                    <a href="service-plumber.php" class="stretched-link text-center">
                         <h3>Plumbing Services</h3>
                     </a>
-                    <p>We offer a wide range of plumbing solutions.</p>
+                    <div class="text-center mb-4">
+                        <p>We offer a wide range of plumbing solutions.</p>
+                    </div>
                 </div>
             </div><!-- End Service Item -->
 
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
                 <div class="service-item position-relative">
-                    <div class="icon">
-                        <i class="bi bi-chat-text" style="color: #20c997;"></i>
+                    <div class="service-img">
+                        <img src="assets/img/service/ac.jpg" alt="ac">
                     </div>
-                    <a href="service-ac.php" class="stretched-link">
+                    <a href="service-ac.php" class="stretched-link text-center">
                         <h3>AC service & Repair</h3>
                     </a>
-                    <p>We ensure your air conditioning system work efficiently.</p>
-                </div>
-            </div><!-- End Service Item -->
-
-            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
-                <div class="service-item position-relative">
-                    <div class="icon">
-                        <i class="bi bi-globe" style="color: #6610f2;"></i>
+                    <div class="text-center mb-4">
+                        <p>We ensure your air conditioning system work efficiently.</p>
                     </div>
-                    <a href="service-details.html" class="stretched-link">
-                        <h3>Velit Doloremque</h3>
-                    </a>
-                    <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
-                </div>
-            </div><!-- End Service Item -->
-
-            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
-                <div class="service-item position-relative">
-                    <div class="icon">
-                        <i class="bi bi-clock" style="color: #f3268c;"></i>
-                    </div>
-                    <a href="service-details.html" class="stretched-link">
-                        <h3>Dolori Architecto</h3>
-                    </a>
-                    <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
                 </div>
             </div><!-- End Service Item -->
 
